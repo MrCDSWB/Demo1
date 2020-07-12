cb86e12ed9493c21dc726ddd076264ab1267eaff
yy <2378020782@qq.com>
2020-07-12 15:04:41 +0800

    登录

--- src/servelt/LoginServlet.java
+++ src/servelt/LoginServlet.java
@@ -14,20 +14,13 @@ import dao.UserDao;
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public LoginServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
-
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // TODO Auto-generated method stub
-//        response.getWriter().append("Served at: ").append(request.getContextPath());
         req.setCharacterEncoding("utf-8");
         resp.setContentType("text/html;charset=utf-8");
         HttpSession session=req.getSession();
@@ -62,9 +55,7 @@ public class LoginServlet extends HttpServlet {
         adminDao.reg(a);
         resp.sendRedirect(req.getContextPath() + "/Sussess.jsp");
     }
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
         doGet(request, response);
